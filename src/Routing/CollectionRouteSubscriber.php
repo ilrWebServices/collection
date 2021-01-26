d46ec5924f0fae194b3a8bab446826755d63bb5c
Jeff Amaral <jeff@freethoughtdesign.com>
2021-01-26 12:41:13 -0500

    Upcast the collection parameter in the collection_item listing view
    This fixes a WSOD when viewing the Items tab on a Collection.
    
    See https://www.drupal.org/project/drupal/issues/2528166
    
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

---
+++ src/Routing/CollectionRouteSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\collection\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class CollectionRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $route_collection) {
+    foreach ($route_collection->all() as $route) {
+      $path = $route->getPath();
+
+      if ($path === '/collection/{collection}/items') {
+        //  @todo Remove if https://www.drupal.org/i/2528166 lands.
+        if (!$route->getOption('parameters')) {
+          $route->setOption('parameters', [
+            'collection' => [
+              'type' => 'entity:collection',
+            ],
+          ]);
+        }
+      }
+    }
+  }
+
+}
